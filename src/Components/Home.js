c2fbcbb5028657218366404b75f92f848d03a092
Sisi Small <smallsianah@gmail.com>
2023-12-19 11:29:41 -0500

    added headshot, fixed landing page bg, and changed styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import LandingBackground from "../Assets/landing-page-background.png"
+import Headshot from "../Assets/headshot.png"
+import { FiArrowRight } from "react-icons/fi"
 
 const Home = () => {
   return (
-    <div class = "home-container">
+    <div class = "home-container"> 
     <Navbar/>
+
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src= {LandingBackground} alt="" />
+        </div>
+      </div>
+
     </div>
   )
 }
