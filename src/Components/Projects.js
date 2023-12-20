19332ea3de3ee890e03d47ae45ca1470c9758cbb
Sisi Small <smallsianah@gmail.com>
2023-12-19 19:27:11 -0500

    created project.js and import images

---
+++ src/Components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BSFillPlayCircleFull } from 'react-icons/bs'
+import i4software from "../Assets/i4software.png"
+import SavvySaver from "../Assets/savvysaver.png"
+import SianahSmall from "../Assets/sianahsmall.png"
+import StudyBuddy from "../Assets/studdybuddy.png"
+
+const Projects = () => {
+  return (
+    <div className="about-section-container">
+        
+    </div>
+  )
+}
+
+export default Projects
