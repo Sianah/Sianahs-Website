1d15debe78b7bf2a25db7904ec8bdc2962f4e2ad
Sisi Small <smallsianah@gmail.com>
2023-12-22 09:52:21 -0600

    beginning to set up routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Home from './Components/Home';
 import Projects from './Components/Projects';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <Projects/>
+      {/* <Home/>
+      <Projects/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route index element = {<Home/>}/>  {/*use index to begin on the main landing page */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
