9406741a02c36d3ff3e54bb683d652d074d7b001
Sisi Small <smallsianah@gmail.com>
2023-12-24 23:35:35 -0600

    copied homepage to contact page for re-formating

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import Navbar from './Navbar';
+import ContactBackground from "../Assets/contact-page-background.png"
+import Headshot from "../Assets/headshot.png"
+import { FiArrowRight } from "react-icons/fi"
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import Projects from './Projects';
+import {Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div>
-      <Navbar/>
+    <div class = "home-container"> 
+    <Navbar/>
+
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src= {ContactBackground} alt="" />
+        </div>
+        
+        <div className="home-text-section">
+          <h1 className="primary-heading" style={{position:'absolute',left:500}}>
+          Contact
+          </h1>
+          <p className="primary-text" style={{position:'absolute',left:400,top:80, }}>
+          My email is smallsianah@gmail.com if you would like to email me about anything pertaining 
+          to software development. You can also contact me through my linkedIn, Instagram, or Github 
+          by pressing the correlating icons below.
+          </p>
+        </div>
+
+      </div>
     </div>
   )
 }
